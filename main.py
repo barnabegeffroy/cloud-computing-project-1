9784e05eccf85b1b47930feb8f97a3af681cdf46
Barnabé Geffroy <barnabe.geffroy@ensiie.fr>
2022-03-07 20:46:11 +0000

    add login system

---
+++ main.py
@@ -0,0 +1,46 @@
+import google.oauth2.id_token
+from flask import Flask, render_template, request, redirect
+from google.auth.transport import requests
+from google.cloud import datastore
+
+app = Flask(__name__)
+datastore_client = datastore.Client()
+firebase_request_adapter = requests.Request()
+
+
+# @app.route('/')
+# def root():
+#     id_token = request.cookies.get("token")
+#     error_message = None
+#     claims = None
+
+#     if id_token:
+#         try:
+#             claims = google.oauth2.id_token.verify_firebase_token(
+#                 id_token, firebase_request_adapter)
+
+#         except ValueError as exc:
+#             error_message = str(exc)
+
+#     return render_template('index.html', user_data=claims, error_mesnsage=error_message)
+
+
+@app.route('/login')
+def root():
+    id_token = request.cookies.get("token")
+    error_message = None
+    claims = None
+
+    if id_token:
+        try:
+            claims = google.oauth2.id_token.verify_firebase_token(
+                id_token, firebase_request_adapter)
+
+        except ValueError as exc:
+            error_message = str(exc)
+
+    return render_template('login.html', user_data=claims, error_mesnsage=error_message)
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
